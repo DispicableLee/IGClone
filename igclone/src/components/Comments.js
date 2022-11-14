ff017b45af992ba18b31cebfbdc6d8447c3976c4
DispicableLee <lee.robert053@gmail.com>
2022-11-14 16:20:38 -0500

    d

--- igclone/src/components/Comments.js
+++ igclone/src/components/Comments.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Typography from '@mui/material/Typography';
-import { linkClasses } from "@mui/material";
+
 
 export default function Comments(props){
     return (
