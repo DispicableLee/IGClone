d328f9f70a5c8684fc5a1e9bbd791b3f25f49d28
DispicableLee <lee.robert053@gmail.com>
2022-11-14 10:56:47 -0500

    done with intial setup

--- igclone/src/App.js
+++ igclone/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+//=========================================================================================
+//================================= components ============================================
+import Post from './components/Post'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+{/*================================== nav bar ============================================*/}
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar 
+        position="static"
+        style={{marginBottom: "30px"}}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Profile
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+{/* ========================================postlist============================================== */}
+
+      <Post/>
+
     </div>
   );
 }
---
+++ igclone/src/components/Comments.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Typography from '@mui/material/Typography';
+
+export default function Comments(){
+    return (
+        <div>
+            <Typography>Hi</Typography>
+        </div>
+    )
+}
